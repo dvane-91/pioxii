374724bf287905728569e5a4ed08354b04112655
Vanessa <vanemelog@gmail.com>
2020-11-12 17:54:26 -0500

    Abrir página de Pio xii

---
+++ cypress/integration/pioxii.spec.js
@@ -0,0 +1,10 @@
+describe('Abrir página de Pio xii', () => {
+    it('Abrir página', () => {
+        cy.visit('https://pioxii.phidias.co/')
+    })
+    it('ingresar usuario y contraseña', ()=> {
+        cy.get('#autofocus').type('diana.melo94')
+        cy.get('fieldset > :nth-child(2) > input').type('coralmega9005')
+        cy.get('[type="submit"]').click()
+    })
+})
